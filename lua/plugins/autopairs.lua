881dc78816ec89a3c903cb997dc7c471835c4b85
vloe <vloe.dev@gmail.com>
2023-06-13 17:03:35 +0200

    make autotag work w cmp (thanks kutsan)

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -3,5 +3,17 @@ if not autopairs then
 	print("autopairs not installed")
 	return
 end
+local status, cmp = pcall(require, "cmp")
+if not status then
+	print("cmp not installed")
+	return
+end
+local status, autopairs_cmp_completion = pcall(require, "nvim-autopairs.completion.cmp")
+if not status then
+	print("nvim-autopairs.completion.cmp not installed")
+	return
+end
 
 autopairs.setup({})
+
+cmp.event:on('confirm_done', autopairs_cmp_completion.on_confirm_done())
